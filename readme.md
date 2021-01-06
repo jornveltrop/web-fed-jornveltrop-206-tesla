b1a83a982922e80bcb458cbcb961640206b0dc3a
jornveltrop <74248007+jornveltrop@users.noreply.github.com>
2021-01-06 23:39:03 +0100

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,83 @@
+# Procesverslag
+**Auteur:** Jorn Veltrop
+**Klas:** 206B
+**Datum:** 6 - 1 - 2021
+**Docent:** Robert Spier
+**Vak:** Front-end development
+**Blok:** Blok 2 Web
+**Schooljaar:** 2
+
+https://jornveltrop.github.io/web-fed-jornveltrop-206-tesla/
+
+
+## Bronnenlijst
+1. Speedometer SVG icon's Tesla: https://www.tesla.com/nl_nl/models
+
+
+
+## Eindgesprek (week 7/8)
+
+Alles is af, ik vond het een super leuk project! Ik heb veel geleerd. Ik zie het elke keer als een leuke uitdaging om het volgende probleem op te lossen **Ik voel mij Nikola Tesla.**
+
+**Screenshots:**
+
+<img src="images/resultaat_1.png" width="375px" alt="Resultaat 1">
+
+<img src="images/resultaat_2.png" width="375px" alt="Resultaat 2">
+
+<img src="images/resultaat_3.png" width="375px" alt="Resultaat 3">
+
+<img src="images/resultaat_4.png" width="375px" alt="Resultaat 4">
+
+<img src="images/resultaat_5.png" width="375px" alt="Resultaat 5">
+
+
+## Voortgang 3 (week 6)
+
+Ik moest een aantal semantische aanpassingen doen. Zoals de sections vervangen voor articles. En nog wat h2's omwisselen met h3's.
+
+Verder ga ik de komende week aan de slag met de laatste dingen van de 2e pagina (Model S). Zoals de geanimeerde Speedometer.
+
+
+
+## Voortgang 2 (week 5)
+
+Weinig op- en aanmerkingen. De opacity scroll-snap is nu ook responsive. Ik moet vooral aan de slag met de 2e pagina. Pagina 1 is nu helemaal af. Responsive & Surface. 
+
+
+
+## Voortgang 1 (week 3)
+
+### Stand van zaken
+
+Qua design gaat alles goed. Er zitten alleen nog wat bugs in de opacity animatie die gelinkt is aan scrollen. Deze is namelijk nog niet responisve. Daarvoor moet ik zelf een formule voor gaan schrijven. **Ik voel mij Albert Einstein.**
+
+**Screenshot:**
+
+<img src="images/vooruitgang_week1.png" width="375px" alt="Vooruitgang week 1">
+Het begint al ergens op te lijken!
+
+
+## Breakdownschets (week 1)
+
+<img src="images/Breakdown-schets_1.jpg" width="375px" alt="Breakdown-schets 1">
+
+<img src="images/Breakdown-schets_2.jpg" width="375px" alt="Breakdown-schets 2">
+
+
+
+## Intake (week 1)
+
+**Je startniveau:** rood
+
+**Je focus:** surface plane (animaties) [En toch ook een beetje responsive. Ik kon het niet laten.]
+
+**Je opdracht:** https://www.tesla.com/
+
+**Screenshot van de eerste pagina (small screen):**
+
+<img src="images/homepage.jpeg" width="375px" alt="Screenshot van de homepage van Tesla">
+
+**Screenshot van de tweede pagina (small screen):**
+
+<img src="images/productpagina.jpeg" width="375px" alt="Screenshot van productpagina van Tesla">
---
+++ scripts/script.js
@@ -0,0 +1,263 @@
+// 1. HAMBURGER MENU
+
+let menu = document.querySelector("header button");
+let nav = document.querySelector("header nav:last-of-type");
+let menuItems = document.querySelectorAll("header nav:last-of-type ul li a");
+let html = document.querySelector("html");
+let main = document.querySelector("main");
+let meerNav = document.querySelector("header nav:last-of-type");
+let i;
+
+for (i = 0; i < menuItems.length; i++) {
+  menuItems[i].setAttribute("tabindex", "-1");
+}
+
+
+// 1.1 LICK MENU OPEN
+
+menu.addEventListener("click", open);
+
+function open() {
+  menu.classList.toggle("open");
+  nav.classList.toggle("showNav");
+  nav.classList.toggle("hideNav");
+  main.classList.toggle("overflow");
+  main.classList.toggle("navOverlay");
+  menu.classList.toggle("filterReset");
+
+  setTimeout(function() {
+    document.activeElement.blur();
+  }, 300);
+
+  // 1.2 SET TABINDEX
+  for (i = 0; i < menuItems.length; i++) {
+    menuItems[i].setAttribute("tabindex", "0");
+  }
+};
+
+
+// 1.3 ESC CLOSE
+
+document.onkeydown = function(evt) {
+  evt = evt || window.event;
+  if (evt.keyCode == 27) {
+    close();
+  }
+};
+
+
+// 1.4 CLICK MENU CLOSE
+
+main.addEventListener("click", close);
+
+function close() {
+  menu.classList.toggle("open", false);
+  nav.classList.toggle("showNav", false);
+  nav.classList.toggle("hideNav", true);
+  main.classList.toggle("overflow", false);
+  main.classList.toggle("navOverlay", false);
+
+  // 1.4.1 RESET TABINDEX
+  for (i = 0; i < menuItems.length; i++) {
+    menuItems[i].setAttribute("tabindex", "-1");
+  }
+}
+
+
+// 2. SPEEDOMETER ANIMATIE
+
+let naald = document.querySelectorAll("#modelS main ul li section g > svg:nth-child(3)");
+let n;
+
+function naaldAni() {
+  for (n = 0; n < naald.length; n++) {
+    naald[n].classList.toggle("naaldAnimatie");
+    naald[n].classList.toggle("naaldReset");
+  }
+}
+
+window.addEventListener('load', function () {
+  naaldAni();
+});
+
+
+// 3. OPACITY SCROLL-SNAP FORMULE
+
+let overlay1 = document.querySelector("#home main > section:nth-child(1) > section");
+let overlay2 = document.querySelector("#home main > section:nth-child(2) > section");
+let overlay3 = document.querySelector("#home main > section:nth-child(3) > section");
+let overlay4 = document.querySelector("#home main > section:nth-child(4) > section");
+let overlay5 = document.querySelector("#home main > section:nth-child(5) > section");
+let overlay6 = document.querySelector("#home main > section:nth-child(6) > section");
+
+let section1 = document.querySelector("#home main > section:nth-child(1)");
+let section2 = document.querySelector("#home main > section:nth-child(2)");
+let section3 = document.querySelector("#home main > section:nth-child(3)");
+let section4 = document.querySelector("#home main > section:nth-child(4)");
+let section5 = document.querySelector("#home main > section:nth-child(5)");
+let section6 = document.querySelector("#home main > section:nth-child(6)");
+
+let footer = document.querySelector("#home footer");
+
+let viewport = window.innerHeight;
+let viewportHalf = window.innerHeight/2;
+let viewportVijfde = window.innerHeight/5;
+let viewportOnder = viewportHalf - viewportVijfde;
+
+if (document.querySelector("#home")) {
+  window.addEventListener('scroll', scrollActions);
+}
+
+function scrollActions() {
+
+  let distance1 = section1.getBoundingClientRect().top;
+  let distance11 = section1.getBoundingClientRect().bottom - viewportHalf;
+  let distance2 = section2.getBoundingClientRect().top;
+  let distance21 = section2.getBoundingClientRect().bottom - viewportHalf;
+  let distance3 = section3.getBoundingClientRect().top;
+  let distance31 = section3.getBoundingClientRect().bottom - viewportHalf;
+  let distance4 = section4.getBoundingClientRect().top;
+  let distance41 = section4.getBoundingClientRect().bottom - viewportHalf;
+  let distance5 = section5.getBoundingClientRect().top;
+  let distance51 = section5.getBoundingClientRect().bottom - viewportHalf;
+  let distance6 = section6.getBoundingClientRect().top;
+
+
+  if (distance1 < viewportHalf) {
+    let procent1 = 1 - (distance1 / viewportHalf);
+    overlay1.style.opacity = procent1;
+    overlay1.style.pointerEvents = "auto";
+  }
+
+  else {
+    overlay1.style.opacity = 0;
+    overlay1.style.display = "none";
+  }
+
+  if (distance11 < viewportOnder) {
+    let procent11 = (distance11 / viewportOnder);
+    overlay1.style.opacity = procent11;
+  }
+
+
+  if (distance2 < viewportHalf) {
+    let procent2 = 1 - (distance2 / viewportHalf);
+    overlay2.style.opacity = procent2;
+    overlay2.style.display = "flex";
+    overlay1.style.pointerEvents = "none";
+  }
+
+  else {
+    overlay2.style.opacity = 0;
+    overlay2.style.display = "none";
+  }
+
+  if (distance21 < viewportOnder) {
+    let procent21 = (distance21 / viewportOnder);
+    overlay2.style.opacity = procent21;
+  }
+
+
+  if (distance3 < viewportHalf) {
+    let procent3 = 1 - (distance3 / viewportHalf);
+    overlay3.style.opacity = procent3;
+    overlay3.style.display = "flex";
+    overlay2.style.display = "none";
+  }
+  else {
+    overlay3.style.opacity = 0;
+    overlay3.style.display = "none";
+  }
+
+  if (distance31 < viewportOnder) {
+    let procent31 = (distance31 / viewportOnder);
+    overlay3.style.opacity = procent31;
+  }
+
+
+  if (distance4 < viewportHalf) {
+    let procent4 = 1 - (distance4 / viewportHalf);
+    overlay4.style.opacity = procent4;
+    overlay4.style.display = "flex";
+    overlay3.style.display = "none";
+  }
+
+  else {
+    overlay4.style.opacity = 0;
+    overlay4.style.display = "none";
+  }
+
+  if (distance41 < viewportOnder) {
+    let procent41 = (distance41 / viewportOnder);
+    overlay4.style.opacity = procent41;
+  }
+
+
+  if (distance5 < viewportHalf) {
+    let procent5 = 1 - (distance5 / viewportHalf);
+    overlay5.style.opacity = procent5;
+    overlay5.style.display = "flex";
+    overlay4.style.display = "none";
+  }
+
+  else {
+    overlay5.style.opacity = 0;
+    overlay5.style.display = "none";
+  }
+
+  if (distance51 < viewportOnder) {
+    let procent51 = (distance51 / viewportOnder);
+    overlay5.style.opacity = procent51;
+  }
+
+
+  if (distance6 < viewportHalf) {
+    const procent6 = 1 - (distance6 / viewportHalf);
+    overlay6.style.opacity = procent6;
+    overlay6.style.display = "flex";
+    overlay5.style.display = "none";
+    footer.style.display = "block";
+  }
+
+  else {
+    overlay6.style.opacity = 0;
+    overlay6.style.display = "none";
+    footer.style.display = "none";
+  }
+}
+
+
+// 4. SCROLL UP ASIDE
+
+let oud = 0;
+let aside = document.querySelector("#modelS main > aside");
+
+if (document.querySelector("#modelS")) {
+
+  window.addEventListener('scroll', function(e) {
+
+    nieuw = window.pageYOffset;
+
+    if (nieuw < 200){
+      console.log("Niks");
+        aside.classList.toggle("showAside", false);
+        aside.classList.toggle("hideAside", true);
+    } 
+
+    else if (oud - nieuw > 0){
+        console.log("Omhoog");
+        aside.classList.toggle("showAside", true);
+        aside.classList.toggle("hideAside", false);
+    }  
+
+    else if (oud - nieuw < 0){
+        console.log("Omlaag");
+        aside.classList.toggle("showAside", false);
+        aside.classList.toggle("hideAside", true);
+    }
+
+    oud = nieuw;
+  });
+}
+
+
